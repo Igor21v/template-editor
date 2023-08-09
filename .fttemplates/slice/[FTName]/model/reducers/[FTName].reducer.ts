a2c67f6fe7bd2fb344a95be276c81f0ffb66664a
Igor Bondarenko <igor21v@mail.ru>
2023-08-09 13:47:45 +0800

    building architecture

---
+++ .fttemplates/slice/[FTName]/model/reducers/[FTName].reducer.ts
@@ -0,0 +1,7 @@
+import { produce } from "immer";
+import { <FTName | capitalize>Schema } from '../types/<FTName | lowercasefirstchar>';
+
+export const <FTName|lowercasefirstchar>Reducer = () => {
+    const nextState = produce();
+    return nextState;
+};
---
+++ .fttemplates/slice/[FTName]/ui/[FTName]/[FTName].tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import cls from './<FTName>.module.scss';
+
+interface <FTName>Props {
+    
+}
+
+export const <FTName> = memo((props: <FTName>Props) => {
+    const {
+        
+    } = props;
+    return (
+        <div className={cls.<FTName>} >
+           
+        </div>
+    );
+});
