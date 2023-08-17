308b3e308f1b7641068b1504886aff58ae23460a
Igor Bondarenko <igor21v@mail.ru>
2023-08-17 11:52:06 +0800

    coverage to gitignore

--- src/widgets/TemplateEditor/model/services/generateMessage/generateMessage.test.ts
+++ src/widgets/TemplateEditor/model/services/generateMessage/generateMessage.test.ts
@@ -1,16 +1,82 @@
 import { generateMessage } from './generateMessage';
-const emptyTemplate = {
-  AFTER: {
-    value: '',
-    next: [],
-  },
-};
 
 describe('Generate Message', () => {
   it('Shuld return empty message when template is empty', () => {
-    const template = emptyTemplate;
+    const template = getEmtyTemplate();
     const values = {};
     const message = generateMessage(template, values);
     expect(message).toBe('');
   });
 });
+
+// constant generation functions
+
+function getEmtyTemplate() {
+  return {
+    AFTER: {
+      value: '',
+      next: [],
+    },
+  };
+}
+function getItinTemplate() {
+  return {
+    AFTER: {
+      value: 'Привет ',
+      next: [
+        {
+          IF: {
+            value: '{firstname}',
+          },
+          THEN: {
+            value: '{firstname}',
+            next: [
+              {
+                IF: {
+                  value: '{lastname}',
+                },
+                THEN: {
+                  value: '{lastname}',
+                  next: [],
+                },
+                ELSE: {
+                  value: '222',
+                  next: [],
+                },
+                AFTER: {
+                  value: '222',
+                  next: [],
+                },
+              },
+              {
+                IF: {
+                  value: 'g',
+                },
+                THEN: {
+                  value: 'nn',
+                  next: [],
+                },
+                ELSE: {
+                  value: '222',
+                  next: [],
+                },
+                AFTER: {
+                  value: 'vvvc',
+                  next: [],
+                },
+              },
+            ],
+          },
+          ELSE: {
+            value: '222xx',
+            next: [],
+          },
+          AFTER: {
+            value: '222ffv',
+            next: [],
+          },
+        },
+      ],
+    },
+  };
+}
